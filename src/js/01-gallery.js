056568ced70bdd9dfca31c0ebf26cbcf7d79a296
johnpaulo27 <138137573+johnpaulo27@users.noreply.github.com>
2024-04-21 02:35:57 +0800

    2nd commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,21 +4,11 @@ import SimpleLightbox from "simplelightbox";
 
 // Additional styles import
 import "simplelightbox/dist/simple-lightbox.min.css";
-document.addEventListener("DOMContentLoaded", function () {
-    // Your previous JavaScript code here
-    function enlargeImage(image) {
-      const enlargedContainer = document.createElement('div');
-      enlargedContainer.classList.add('enlarged');
-      enlargedContainer.onclick = () => document.body.removeChild(enlargedContainer);
 
-      const enlargedImage = document.createElement('img');
-      enlargedImage.src = image.src;
-
-      enlargedContainer.appendChild(enlargedImage);
-      document.body.appendChild(enlargedContainer);
-    }
-    // Example usage of SimpleLightbox
-    const gallery = new SimpleLightbox('.gallery a');
+image.addEventListener('click', () => {
+  image.classList.toggle('zoomed');
 });
-
-console.log(galleryItems);
+function toggleImageVisibility() {
+  var image = document.getElementById('image');
+  image.classList.toggle('hidden');
+}
